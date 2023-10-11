4494402adbb81607c10ea2dbd63d2193343f4394
p421199 <gareth.ireson@nationwide.co.uk>
2023-10-11 08:07:29 +0100

    issue parsing props for top position to affect the note placement on the stave in the DOM

--- src/app.tsx
+++ src/app.tsx
@@ -2,36 +2,60 @@ import { useState } from 'preact/hooks'
 import stave from './assets/stave.svg'
 import crotchet from './assets/crotchet.svg'
 import './app.css'
+import styled from 'styled-components'
+import { css } from 'styled-components'
 
 export function App() {
-  let notes: string[] = ["noteLowE","noteLowF", "noteG", "noteA", "noteB", "noteC", "noteD", "noteHighE", "noteHighF"]
-  
+
+  const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`
+
+  const Button = styled.button`
+  position: absolute;
+  top: 40em;
+  left: 36em;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #BF4F74;
+  border-radius: 3px;
+`
+
+  const StyledStave = styled.img`
+    position: relative;
+    height: 50em;
+    width: 50em;
+  `
+
+  const StyledNote = styled.img.attrs(props => ({
+    $note: props.$note
+  })) <{ $note?: string; }>`
+    position: absolute;
+    left: 35em;
+    top: ${props => props.$note};
+    height: 12em;
+  `
+
+// all top position values for the note to be placed on the stave from Low E to High F respectively
+  let notes: string[] = ["26em", "24.7em", "23.2em", "21.9em", "20.5em", "19.1em", "17.8em", "16.4em", "15em"]
+
   let [count, setCount] = useState(0)
 
   function nextNote() {
-    if (count < notes.length-1){
+    if (count < notes.length - 1) {
       return setCount(count + 1)
     } else {
       return setCount(0)
     }
   }
-
   return (
-    <>
-      <div id="parent">
-        
-          <img src={stave} class="stave" alt="stave" />
-          <img src={crotchet} class={notes[count]} alt="crotchet" />
-        
-      </div>
-      <div class="card">
-        <button onClick={() => nextNote()}>
-          Click to change note
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
+
+    <Wrapper>
+      <StyledStave src={stave} />
+      <StyledNote note={notes[count]} src={crotchet} />
+      <Button onClick={() => nextNote()}>Next Note</Button>
+    </Wrapper>
   )
 }
